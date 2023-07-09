5596f21603ab8b587cb96b4d426462a605f29432
Nerd <imalonelynerd@gmail.com>
2023-07-09 19:47:43 +0200

    Theme update !

---
+++ js/darkmode.js
@@ -0,0 +1,25 @@
+function switchTheme(theme) {
+    $("#blahaj").attr("src", `im/logo-vert-${theme}.png`);
+    document.querySelector('html').dataset.theme = `theme-${theme}`;
+    document.cookie = `theme=${theme}`;
+}
+
+function switchThemeAlt() {
+    if (document.cookie === "theme=dark") {
+        switchTheme("light");
+    } else {
+        switchTheme("dark");
+    }
+}
+
+switch (document.cookie) {
+    case "theme=dark":
+        switchTheme("dark");
+        break;
+    case "theme=light":
+        switchTheme("light");
+    default:
+        const isOsDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+        switchTheme(isOsDark?"dark":"light");
+        break;
+}
