abc5e65adbb92f5d255f2aad946f77d5716317da
Nerd <imalonelynerd@gmail.com>
2023-09-01 11:39:49 +0200

    Funny system bug 2 : JS Boogaloo

--- js/darkmode.js
+++ js/darkmode.js
@@ -1,23 +1,25 @@
 function switchTheme(theme) {
-    if (theme === "system") {
-        const isOsDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
-        switchTheme(isOsDark ? "dark" : "light");
-    } else {
-        $("#blahaj").attr("src", `img/logo-${theme}.png`);
-        document.querySelector('html').dataset.theme = `theme-${theme}`;
-        document.cookie = `theme=${theme}`;
+    switch (theme) {
+        case "system":
+            const isOsDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+            switchTheme(isOsDark ? "dark" : "light");
+            break;
+        case "light":
+            $("#blahaj").attr("src", `img/logo-light.png`);
+            document.querySelector('html').dataset.theme = `theme-light`;
+            document.cookie = `theme=light`;
+            break;
+        case "dark":
+            $("#blahaj").attr("src", `img/logo-dark.png`);
+            document.querySelector('html').dataset.theme = `theme-dark`;
+            document.cookie = `theme=dark`;
+            break;
+        default:
+            break;
     }
     $(".menu#m1").hide();
     return true;
 }
-
-/*function switchThemeAlt() {
-    if (document.cookie === "theme=dark") {
-        switchTheme("light");
-    } else {
-        switchTheme("dark");
-    }
-}*/
 
 switch (document.cookie) {
     case "theme=light":
